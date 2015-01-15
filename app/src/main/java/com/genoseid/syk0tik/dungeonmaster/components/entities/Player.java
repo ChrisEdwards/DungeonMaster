fd4b246ba56790b4400fcfa6dc9ca50a76c972e1
Promethi0s <nic.j.gibson@gmail.com>
2015-01-14 19:02:52 -0600

    Update Player.java

--- app/src/main/java/com/genoseid/syk0tik/dungeonmaster/components/entities/Player.java
+++ app/src/main/java/com/genoseid/syk0tik/dungeonmaster/components/entities/Player.java
@@ -1,5 +1,11 @@
 package com.genoseid.syk0tik.dungeonmaster.components.entities;
 
 public class Player {
-
+  public Player(int xStart, int yStart) {
+    xLoc = xStart;
+    yLoc = yStart;
+  }
+  public void update(Map level, EntityMap entities, List<TouchEvent> touchEvents)) {
+    
+  }
 }
